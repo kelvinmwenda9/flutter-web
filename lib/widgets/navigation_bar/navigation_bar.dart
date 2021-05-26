a02799621c5bb337fa6a2aadcec50b02301e4f93
Kelvin Kaburu <kelvinmwenda9@gmail.com>
2021-05-26 13:32:28 +0300

    flutter web app

---
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:the_basics/widgets/navigation_bar/navbar_item.dart';
+import 'package:the_basics/widgets/navigation_bar/navbar_logo.dart';
+import 'package:the_basics/widgets/navigation_bar/navigation_bar_mobile.dart';
+import 'package:the_basics/widgets/navigation_bar/navigation_bar_tablet_desktop.dart';
+
+class NavigationBar extends StatelessWidget {
+  const NavigationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+      mobile: NavigationBarMobile(),
+      tablet: NavigationBarTabletDesktop(),
+    );
+  }
+}
